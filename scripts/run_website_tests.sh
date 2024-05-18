a343a805af1d1334f843975d656613c2e8a4cbe3
Sergey Lazarev <lasest1999@gmail.com>
2024-05-18 19:26:31 +0500

    Fixed path

--- scripts/run_website_tests.sh
+++ scripts/run_website_tests.sh
@@ -3,6 +3,6 @@
 SCRIPTS_DIR="$(dirname "$(realpath "$0")")"
 PROJECT_ROOT="$(dirname "$SCRIPTS_DIR")"
 
-cd $PROJECT_ROOT &&
+cd "$PROJECT_ROOT" &&
 source ./.venv/bin/activate &&
 pytest --alluredir ./app_data/reports ./src/simbirsoft/tests/test_website.py
